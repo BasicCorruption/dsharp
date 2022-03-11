9a7c7073af73c4819223af8fd6d7742ccad403c1
SidMinecraft <68089579+BasicCorruption@users.noreply.github.com>
2022-03-10 20:57:28 -0500

    Update main.lua

---
+++ src/main.lua
@@ -0,0 +1,18 @@
+_G.COMMAND_TABLE = {
+    "println": {
+        "desc": "Prints a line of text to the console or window",
+        "args": [
+            {
+                "name": "Text",
+                "type": "String",
+                "required": true
+            }
+        ]
+    }
+};
+
+_G.FUNCTIONS = {
+    "println": function ( Text : String )
+        -- body
+    end
+}
